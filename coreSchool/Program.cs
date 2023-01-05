0fb4a1a586e8cf520e4e1520df7515386df2da2b
juansilva <juandsr@hotmail.com>
2023-01-04 20:57:24 -0500

    Add project files.

---
+++ coreSchool/Program.cs
@@ -0,0 +1,21 @@
+﻿using coreSchool.entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coreSchool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var escuela = new Escuela("Platzi Academic", 2012);
+            escuela.Pais = "Colombia";
+            escuela.Ciudad = "Bogota"; 
+
+            Console.WriteLine(escuela.Nombre + " " + escuela.AñoDeCreacion);
+        }
+    }
+}
---
+++ coreSchool/entidades/Escuela.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coreSchool.entidades
+{
+    internal class Escuela
+    {
+        public string Nombre { get; set; }
+
+        public string Pais { get; set; }
+
+        public int AñoDeCreacion { get; set; }
+
+        public string Ciudad { get; set; }
+
+        public Escuela(string nombre, int añoDeCreacion)
+        {
+            Nombre = nombre;
+            AñoDeCreacion = añoDeCreacion;
+        }
+    }
+}
